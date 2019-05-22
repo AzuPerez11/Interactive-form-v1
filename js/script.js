fc93e470fcab8ea0febe0f64da757267e17ec957
AzuPerez11 <az_perez11@hotmail.com>
2019-05-21 21:45:25 -0700

    Task 2 completed
    The T-shirt function works!

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
 //testing local changes to GitHub 
-
+$("#name").focus();
 //Include a text field that will be revealed with "other" is selected
 
-
-const $newText = $("#other-title")
+const $newText = $("#other-title");
  $($newText).hide();
-$("#title").change(function(){
-if($("#title").val()==="other"){
-    $($newText).show();
-} else {
-    $($newText).hide()
-}
+    $("#title").change(function(){
+        if($("#title").val()==="other"){
+         $($newText).show();
+        } else {
+        $($newText).hide()
+    }
 });
 
 //For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
 
+ const $puns = $("#design option:eq(1)");
+ const $heart = $("#design option:eq(2)");
+ const $punsColors = $("#color option:lt(3)");
+ const $heartColors = $("#color option:gt(2)");
+
+$("#color").hide();
+    $("#design").change(function(){
+        if($("#design").val()==="js puns"){
+            $("#color").show();
+            $($heartColors).hide();
+         $($punsColors).show()
+        } else {
+        $($punsColors).hide()
+    }
+    });
+
+    $("#color").hide();
+    $("#design").change(function(){
+        if($("#design").val()==="heart js"){
+            $("#color").show();
+            $($punsColors).hide();
+         $($heartColors).show()
+        } else {
+        $($heartColors).hide()
+    }
+    });
 
 
-//If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
 
+    
 //The form element:
 //<form action = "index.html" method = "post"></form>
 //<h1>Sign Up</h1>
